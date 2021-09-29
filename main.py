0ac80eed1a078ff71dd605c3ab3470bdc8e08bc9
SNOW <74203547+ByFocus@users.noreply.github.com>
2021-09-29 19:39:36 +0200

    Upgraded main.py
    I've added a color picker for choosing the light colors and i fixed some bugs that i had

--- main.py
+++ main.py
@@ -2,49 +2,72 @@ import dearpygui.dearpygui as dpg
 import requests
 import json
 import token
-selector = "all"
 headers = {"Authorization": "Bearer %s" % token,}
+selector = "all"
+changeColor = ""
 
 def selector_changer(sender):
     global selector
-    if sender == 40:
+    if sender == 48:
         selector = "all"
-    if sender == 42:
-        selector = "Here goes your id light"
-    if sender == 43:
-        selector = "Here goes your id light"
-    if sender == 44:
-        selector = "Here goes your id light"
-    if sender == 45:
-        selector = "Here goes your id light"
-    print(selector)
+        print("You've chosen all lights") 
+    if sender == 50:
+        selector = "id:d073d5560686"
+        print("You've chosen light numer 1") 
+    if sender == 51:
+        selector = "id:d073d555c957"
+        print("You've chosen light numer 2") 
+    if sender == 52:
+        selector = "id:d073d555b26b"
+        print("You've chosen light numer 3") 
+    if sender == 53:
+        selector = "id:d073d5560563"
+        print("You've chosen light numer 4") 
 
 def color_changer(sender):
-    if sender == 31:
+    global changeColor
+    if sender == 35:
         changeColor = 'blue'
         print('Changing to blue...')
-    if sender == 32:
+    if sender == 36:
         changeColor = 'purple'
-        print('Changing to purple...')
-    if sender == 33:
+        print('Changing yo purple...')
+    if sender == 38:
         changeColor = 'red'
         print('Changing to red...')
-    if sender == 34:
+    if sender == 39:
         changeColor = 'cyan'
         print('Changing to cyan...')
-    if sender == 35:
+    if sender == 41:
         changeColor = 'white'
         print('Changing to default...')
+    if sender == 105:
+        print(f'Changing to {changeColor}')
     payload = {
-        "states": [{
+        "states": [
+            {
         "selector": selector,
         "power": "on",
         "color": changeColor
-            }
+            },
+           
         ]
     }
     response = requests.put('https://api.lifx.com/v1/lights/states', data=json.dumps(payload), headers=headers)
-    print(selector)
+
+
+def get_color_value(): 
+    global changeColor
+    getColorValue = dpg.get_value(colorPicker)
+    firstColorRounded = int(getColorValue[0])
+    secondColorRounded = int(getColorValue[1])
+    thirdColorRounded = int(getColorValue[2])
+    roundedColors = (f'{firstColorRounded},{secondColorRounded},{thirdColorRounded}')
+    print(roundedColors)
+    changeColor = (f'rgb:{roundedColors}')
+    color_changer(sender=105)
+    
+
 
 def lights_controll(sender):
     if sender == 26:
@@ -57,10 +80,9 @@ def lights_controll(sender):
             {
             "selector": selector,
             "power": petition,
-            }
+            },
         ],
     }
-    print(selector)
 
     response = requests.put('https://api.lifx.com/v1/lights/states', data=json.dumps(payload), headers=headers)
 
@@ -80,7 +102,10 @@ def sender_value_brightness():
       {
           "selector" : selector,
           "brightness": brightness_value
-  }]}
+    },
+    
+        ]
+  }
   response = requests.put('https://api.lifx.com/v1/lights/states', data=json.dumps(payload), headers=headers)
 
 
@@ -102,29 +127,38 @@ with dpg.window(label="Brightness", pos=(100,0), width=300, height=100) as windo
     #sendBrightness = dpg.add_button(label="Send info")
     sliderBrightness = dpg.add_slider_float(label="Brightness", max_value=1, min_value=0, default_value=1) 
     send_info = dpg.add_button(label="Send Info!", callback=sender_value_brightness)
+    
 
 
 
 # 3 Window
 
-with dpg.window(label="Color", pos=(400, 0)) as window2:
-    blueBtn = dpg.add_button(label="Blue", callback= color_changer) #sender = 31
-    purpleBtn = dpg.add_button(label="Purple", callback= color_changer) # sender = 32
-    redBtn = dpg.add_button(label="Red", callback= color_changer)# sender = 33
-    cyanBtn = dpg.add_button(label="Cyan", callback= color_changer)# sender = 34
-    whiteBtn = dpg.add_button(label="White", callback= color_changer)# sender = 35
+with dpg.window(label="Color", pos=(0, 100), height=600, width=400) as window2:
+    with dpg.table(header_row=False):    
+        dpg.add_table_column()
+        dpg.add_table_column()
+        dpg.add_table_column()
+        blueBtn = dpg.add_button(label="Blue", callback= color_changer) #sender = 35
+        purpleBtn = dpg.add_button(label="Purple", callback= color_changer) # sender = 36
+        dpg.add_table_next_column()
+        redBtn = dpg.add_button(label="Red", callback= color_changer)# sender = 38
+        cyanBtn = dpg.add_button(label="Cyan", callback= color_changer)# sender = 39
+        dpg.add_table_next_column()
+        whiteBtn = dpg.add_button(label="White", callback= color_changer)# sender = 41
+    colorPicker = dpg.add_color_picker(label="Choose a color")
+    confirmColor = dpg.add_button(label="Confirm", callback=get_color_value)
 
     # 4 Window
-with dpg.window(label='Light Selector', pos=(500, 0), width=200):
+with dpg.window(label='Light Selector', pos=(400, 0), width=200):
     with dpg.table(header_row=False):
         dpg.add_table_column()
         dpg.add_table_column()
-        selectorAll = dpg.add_button(label="All", callback=selector_changer) #sender = 40
+        selectorAll = dpg.add_button(label="All", callback=selector_changer) #sender = 48
         dpg.add_table_next_column()
-        selectNumberOne = dpg.add_button(label="1", width=30, callback=selector_changer) #sender = 42
-        selectNumberTwo = dpg.add_button(label="2", width=30, callback=selector_changer) #sender = 43
-        selectNumberThree = dpg.add_button(label="3", width=30, callback=selector_changer) #sender = 44
-        selectNumberFour = dpg.add_button(label="4", width=30, callback=selector_changer) #sender = 45
+        selectNumberOne = dpg.add_button(label="1", width=30, callback=selector_changer) #sender = 50
+        selectNumberTwo = dpg.add_button(label="2", width=30, callback=selector_changer) #sender = 51
+        selectNumberThree = dpg.add_button(label="3", width=30, callback=selector_changer) #sender = 52
+        selectNumberFour = dpg.add_button(label="4", width=30, callback=selector_changer) #sender = 53
 
     
 
@@ -133,7 +167,7 @@ with dpg.window(label='Light Selector', pos=(500, 0), width=200):
 
 dpg.setup_viewport()
 dpg.set_viewport_title(title='Lifx Controller Lights')
-dpg.set_viewport_width(750)
-dpg.set_viewport_height(200)
+dpg.set_viewport_width(617)
+dpg.set_viewport_height(740)
 
 dpg.start_dearpygui()
